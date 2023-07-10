d00f6e403d6c4543a8545b9e6bfaf1dd44d95b90
Dastan <dastiw1010@gmail.com>
2023-07-10 15:11:57 +0600

    Added views, urls

---
+++ apps/academy/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from apps.academy.views import IndexView,  TeacherDetailView, CourseDetailView
+
+urlpatterns = [
+    path("", IndexView.as_view(), name='index'),
+    path('teacher/<int:teacher_id>/', TeacherDetailView.as_view(), name='teacher_detail'),
+    path('course/<int:course_id>/', CourseDetailView.as_view(), name='course_detail'),
+    ]
--- apps/academy/views.py
+++ apps/academy/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 
-# Create your views here.
+from apps.academy.models import Teacher, Course
+
+
+class IndexView(View):
+    def get(self, request):
+        context = {'title': 'Главная страница'}
+        return render(request, 'academy/index.html', context)
+
+
+class TeacherDetailView(View):
+    def get(self, request, teacher_id):
+        teacher = get_object_or_404(Teacher, id=teacher_id)
+        context = {
+            'title': f'{teacher.full_name} - преподаватель',
+            'teacher': teacher,
+        }
+        return render(request, 'academy/teacher_detail.html', context)
+
+
+class CourseDetailView(View):
+    def get(self, request, course_id):
+        course = get_object_or_404(Course, id=course_id)
+        context = {
+            'title': f'{course.name} - курс',
+            'course': course,
+        }
+        return render(request, 'academy/course_detail.html', context)
+
